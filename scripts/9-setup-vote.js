c1728808c755f067488f971ea6e0f95027150148
Sidhanti Patil <sidpatil2209@gmail.com>
2022-01-11 22:09:49 +0530

    idk

--- scripts/9-setup-vote.js
+++ scripts/9-setup-vote.js
@@ -6,25 +6,42 @@ const voteModule = sdk.getVoteModule("0xFe4Fdde74a791526373a307d3C4f73f6642bf094
 
 const tokenModule = sdk.getTokenModule("0x9Ec14fE8607d2EB7D1D3490E1ce061146e4dF0Bd");
 
-(async() => {
-    try{
-        await tokenModule.grantRole("minter", voteModule.address);
-        console.log("Successfully gave vote module permission to act on token module");
-    }catch(err){
-        console.error("Failed to grant vote module permissions on token module", err);
-        process.exit(1);
+(async () => {
+    try {
+      // Give our treasury the power to mint additional token if needed.
+      await tokenModule.grantRole("minter", voteModule.address);
+  
+      console.log(
+        "Successfully gave vote module permissions to act on token module"
+      );
+    } catch (error) {
+      console.error(
+        "failed to grant vote module permissions on token module",
+        error
+      );
+      process.exit(1);
     }
-
-    try{
-        const ownedTokenBalance = await tokenModule.balanceOf(process.env.WALLET_ADDRESS);
-
-        const ownedAmount = ethers.BigNumber.from(ownedTokenBalance.value);
-        const percent90 = ownedAmount.div(100).mul(90);
-
-        await tokenModule.transfer(voteModule.address, percent90);
-
-        console.log("✅ Successfully transferred tokens to vote module");
-    }catch(err){
-        console.error("failed to transfer tokens to vote module", err);
+  
+    try {
+      // Grab our wallet's token balance, remember -- we hold basically the entire supply right now!
+      const ownedTokenBalance = await tokenModule.balanceOf(
+        // The wallet address stored in your env file or Secrets section of Repl
+        process.env.WALLET_ADDRESS
+      );
+  
+      // Grab 90% of the supply that we hold.
+      const ownedAmount = ethers.BigNumber.from(ownedTokenBalance.value);
+      const percent90 = ownedAmount.div(100).mul(90);
+  
+      // Transfer 90% of the supply to our voting contract.
+      await tokenModule.transfer(
+        voteModule.address,
+        percent90
+      );
+  
+      console.log("✅ Successfully transferred tokens to vote module");
+    } catch (err) {
+      console.error("failed to transfer tokens to vote module", err);
     }
-})();
+  })();
+  
